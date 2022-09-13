65d0268de00efa987ecbdd009b26b0409ed423bf
agustinjaureguiberry <aaguus.mdq92@gmail.com>
2022-09-13 20:34:19 -0300

    Agrego contexto carrito

--- src/components/ItemDetailContainer/Item/Item.jsx
+++ src/components/ItemDetailContainer/Item/Item.jsx
@@ -1,21 +1,31 @@
+
 /* eslint-disable jsx-a11y/anchor-is-valid */
+import { useContext, useState } from "react"
+import { CartContext } from "../../Contextos/CartContext"
 import { ItemCantidad } from "./ItemCantidad/ItemCantidad"
 import { BotonesItem } from "./BotonesItem/BotonesItem"
-import { useState } from 'react'
 
 export const Item = ({ item }) => {
 
+    const { cart, setCart, enCart, sumaCant } = useContext(CartContext)
 
     const [cantidad, setCantidad] = useState(1)
+
     const handleAgCarrito = () => {
         const itemCarrito = {
             cod: item.cod,
             descripcion: item.descripcion,
             precio: item.precio,
-            cantidad
+            cantidad,
+            stock: (item.stock - cantidad)
         }
-        console.log(itemCarrito)
-        alert(`Agregaste nuevo producto, revise la consola`)
+        if (enCart(itemCarrito)) {
+            sumaCant(itemCarrito)
+            console.log(cart)
+        } else {
+            setCart([...cart, itemCarrito])
+            console.log(cart)
+        }
     }
 
     return (
